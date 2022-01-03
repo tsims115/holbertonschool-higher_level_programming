886b8f610745e57e358b44c60346413b459a6859
tsims115 <tsims115@yahoo.com>
2022-01-02 23:08:19 -0800

    fixing task 2

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if (number > 0):
-    print("is positve")
+    print("{} is positve".format(number))
 elif (number < 0):
-    print("is negative")
+    print("{} is negative".format(number))
 else:
-    print("is zero")
+    print("{} is zero".format(number))
