2b69c1482611b6ddac7acd5ded31b36eed4e371a
tsims115 <tsims115@yahoo.com>
2022-01-04 07:57:34 -0800

    Adding task 2

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print("Last digit of {} is ".format(number), end="")
+if number < 0:
+    number = number * -1
+number = number % 10
+if number > 5:
+    print("{} and is greater than 5".format(number))
+elif number == 0:
+    print("{} and is zero".format(number))
+elif number < 6:
+    print("{} and is less than 6 and not zero".format(number))
