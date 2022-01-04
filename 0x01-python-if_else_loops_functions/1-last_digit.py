abfa3f605ce9a8eefbd125a4520d155871083ea5
tsims115 <tsims115@yahoo.com>
2022-01-04 11:00:03 -0800

    Making a change to check for negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 print("Last digit of {} is ".format(number), end="")
 if number < 0:
-    number = number * -1
-number = number % 10
+    number = (number * -1 % 10) * -1
+else:
+    number = number % 10
 if number > 5:
     print("{} and is greater than 5".format(number))
 elif number == 0:
