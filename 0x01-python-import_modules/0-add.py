8099e1c60cecc197c777a60b96a5a2c386119987
tsims115 <tsims115@yahoo.com>
2022-01-05 09:37:21 -0800

    making the def a main call

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def main():
     add = __import__('add_0').add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    main()
     
+
 
