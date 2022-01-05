dff82ec1b13305dca9197749532442c15e997552
tsims115 <tsims115@yahoo.com>
2022-01-05 09:32:34 -0800

    Changing so it will not run when imported

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    add = __import__('add_0').add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    
+
