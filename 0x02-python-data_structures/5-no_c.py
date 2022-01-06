83caf2fa81ccf3ed1b2bae83f3807c52d93c28e4
tsims115 <tsims115@yahoo.com>
2022-01-06 21:57:32 +0000

    Adding a check for empty string

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     idx = 0
+    if my_string == "":
+        return
     for c in my_string:
         if c == 'c' or c == 'C':
             my_string = my_string[:idx] + my_string[idx + 1:]
