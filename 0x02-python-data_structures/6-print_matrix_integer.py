8ab418a11ff0ed8582020ad470a3ef09567f057c
tsims115 <tsims115@yahoo.com>
2022-01-06 23:02:42 +0000

    now it does not print space at the end

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end=" ")
+        for i in range(0, len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i < (len(row) - 1):
+                print("", end=" ")
         print()
