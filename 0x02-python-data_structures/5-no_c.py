a544010a2596306bbe4934e071719b458ab60f85
tsims115 <tsims115@yahoo.com>
2022-01-06 22:20:21 +0000

    Chaning task 5 to make a copy of the string

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    idx = 0
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            my_string = my_string[:idx] + my_string[idx + 1:]
-        idx += 1
-    return my_string
+    new_string = my_string
+    for i in range(0, len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            new_string =  my_string[:i] + my_string[i + 1:]
+    return new_string
