5edc79a734f277e2b1fa63015766aba541efb9c8
tsims115 <tsims115@yahoo.com>
2022-01-10 21:57:52 +0000

    Making code shorter

--- 0x03-python-more_data_structures/4-only_diff_elements.py
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = set()
-    if not set_1 and not set_2:
-        return None
-    for i in set_1:
-        if i in set_2:
-            continue
-        else:
-            new_set.add(i)
-    for j in set_2:
-        if j in set_1:
-            continue
-        else:
-            new_set.add(j)
-    return new_set
+    return set_1 ^ set_2
