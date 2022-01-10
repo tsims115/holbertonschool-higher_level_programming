b33c49b10a2265975f7ce498eed9f8dfefcf8cb4
tsims115 <tsims115@yahoo.com>
2022-01-10 23:14:41 +0000

    chaning check

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     best_score = 0
-    if a_dictionary is None or a_dictionary == {}:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for key, value in a_dictionary.items():
         if value > best_score:
