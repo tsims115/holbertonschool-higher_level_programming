4e62e58039ae6029f15a319f1cf7cf1fdf215a4a
tsims115 <tsims115@yahoo.com>
2022-01-10 19:33:21 +0000

    Adding task 4

---
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new_set = set()
+    for i in set_1:
+        for j in set_2:
+            if i in set_2:
+                continue
+            else:
+                new_set.add(i)
+            if j in set_1:
+                continue
+            else:
+                new_set.add(j)
+    return new_set
+    
