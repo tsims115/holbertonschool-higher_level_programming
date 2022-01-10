f76231a066423f32d3d6de680d600ca7d7294b30
tsims115 <tsims115@yahoo.com>
2022-01-10 23:11:38 +0000

    Adding checks to code

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     count = 0
+    best_score = 0
     if a_dictionary is None:
         return None
     for key, value in a_dictionary.items():
-        if count == 0:
-            best_score = value
+        print("{} {}".format(key, value))
         if value > best_score:
             best_score = value
-    if best_score == -1:
+    if best_score == 0:
         return None
     return best_score
