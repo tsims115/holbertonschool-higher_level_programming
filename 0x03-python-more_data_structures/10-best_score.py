3e538b656e0695ec12d0ce31eeaf5afa7e179bff
tsims115 <tsims115@yahoo.com>
2022-01-10 23:13:44 +0000

    Taking out print

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    count = 0
     best_score = 0
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     for key, value in a_dictionary.items():
         if value > best_score:
             best_score = value
-    if best_score == 0:
-        return None
     return best_score
