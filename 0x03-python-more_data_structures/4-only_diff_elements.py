1df353af258801ee41c80d881a91bc63930b6424
tsims115 <tsims115@yahoo.com>
2022-01-10 21:46:54 +0000

    Adding a check for empty sets

--- 0x03-python-more_data_structures/4-only_diff_elements.py
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = set()
+    if not set_1 or not set_2:
+        return None
     for i in set_1:
-        for j in set_2:
-            if i in set_2:
-                continue
-            else:
-                new_set.add(i)
-            if j in set_1:
-                continue
-            else:
-                new_set.add(j)
+        if i in set_2:
+            continue
+        else:
+            new_set.add(i)
+    for j in set_2: 
+        if j in set_1:
+            continue
+        else:
+            new_set.add(j)
     return new_set
