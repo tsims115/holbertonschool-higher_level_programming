7b38ad79fd7eb4a1348e51e5d54152ae61e82b9c
tsims115 <tsims115@yahoo.com>
2022-01-11 21:55:38 +0000

    Adding task 4

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Class Square based on size with getter and setter methods"""
+
+
+from typing import Type
+
+
+class Square:
+    """Class Square based on size"""
+
+    def __init__(self, size=0):
+        self.size = size
+    
+    def area(self):
+        return self.__size * self.__size
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
