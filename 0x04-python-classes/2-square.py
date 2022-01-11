dc32a3b2c46f6344c2f637caf1b4d0d0649b9d12
tsims115 <tsims115@yahoo.com>
2022-01-11 18:48:30 +0000

    Fixing starting size and pepe

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -4,13 +4,13 @@
 
 class Square:
     """Class Square based on size"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    
+
     def area(self):
         return self.__size * self.__size
