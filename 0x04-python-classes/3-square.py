b31ec4a8e115ecb8849ae474320b24bbdf412bb9
tsims115 <tsims115@yahoo.com>
2022-01-11 21:21:26 +0000

    Adding task 3

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Class Square based on size"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
