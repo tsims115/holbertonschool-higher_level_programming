d0ddc81f49387853ca7e49face4330c7f83b25da
tsims115 <tsims115@yahoo.com>
2022-01-11 00:58:59 +0000

    Chaning it

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -6,11 +6,11 @@ def roman_to_int(roman_string):
     nums = {'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
     for i in range(len(roman_string)):
-        if i == len(roman_string) - 1:
-            value = value + nums[roman_string[i]]
-            break
-        if nums[roman_string[i]] < nums[roman_string[i+1]]:
-            value = value + -nums[roman_string[i]]
-        else:
+        try:
+            if nums[roman_string[i]] < nums[roman_string[i+1]]:
+                value = value + -nums[roman_string[i]]
+            else:
+                value = value + nums[roman_string[i]]
+        except IndexError:
             value = value + nums[roman_string[i]]
     return value
