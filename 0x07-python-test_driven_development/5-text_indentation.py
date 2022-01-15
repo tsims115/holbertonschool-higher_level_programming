400e5fe4dd1ccb734dc7fcea4a3633e6b3c807e5
tsims115 <tsims115@yahoo.com>
2022-01-14 20:50:35 -0800

    Adding changes to task 4 and adding task 4 doctest file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def text_indentation(text):
     flag = 0
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     for c in text:
         if flag == 1:
             flag = 0
             if c == ' ':
                 continue
         if c == '?' or c == '.' or c == ':':
-            flag = 1
             print("{}".format(c))
+            flag = 1
             continue
         print("{}".format(c), end="")
+    print()
