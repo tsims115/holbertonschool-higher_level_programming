be049575db231c4460d57ef12d6ff125582de898
tsims115 <tsims115@yahoo.com>
2022-01-15 21:58:11 -0800

    Adding task 3

---
+++ 0x06-python-more_classes/3-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Module 2-rectangle with class rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle based on height and width
+        width = width of rectangle
+        height = height of rectangle
+        has area and perimeter methods and a str representation
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__height * 2) + (self.__width * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        row = self.__width * "#"
+        return (row + "\n") * (self.__height - 1) + row
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
