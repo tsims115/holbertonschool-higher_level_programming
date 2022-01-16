b2c0fdff39ba5ace6922033bb8adac0dcb2b31d7
tsims115 <tsims115@yahoo.com>
2022-01-15 21:17:16 -0800

    Adding task 1

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Class Rectangle based on length and width"""
+
+
+class Rectangle:
+    """Class rectangle
+    width: width of rectangle
+    height: height of rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+    @property
+    def height(self):
+        return self.__height
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
