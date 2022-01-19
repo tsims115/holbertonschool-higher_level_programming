db1fc6a495f2880c8cbfee4186855b2da0f5f003
tsims115 <tsims115@yahoo.com>
2022-01-19 07:45:36 -0800

    Adding test for float overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,6 +10,7 @@ def add_integer(a, b=98):
         b: can be a float or int or not even given
         Return: the sum of a and b
     """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
