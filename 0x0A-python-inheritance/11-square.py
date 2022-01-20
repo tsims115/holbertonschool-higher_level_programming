dcc384b24dc1a98c17f2bdb8290213dcb1dab311
tsims115 <tsims115@yahoo.com>
2022-01-20 15:55:56 -0800

    Adding task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 10-square with class Square"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+class Square(Rectangle):
+    """Class Square inherits from Rectangle"""
+    def __init__(self, size):
+        if super().integer_validator("size", size):
+            self.__size = size
+            super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
