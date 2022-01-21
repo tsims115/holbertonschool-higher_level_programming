016fd9aab29bdb93741897267c6ed082d06b7814
tsims115 <tsims115@yahoo.com>
2022-01-20 17:56:27 -0800

    Adding changes

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,9 +6,11 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """Class Square inherits from Rectangle"""
     def __init__(self, size):
-        if super().integer_validator("size", size):
-            self.__size = size
-            super().__init__(size, size)
+        """initializes obj with size"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
     def __str__(self):
+        """string representation of square object"""
         return "[Square] {}/{}".format(self.__size, self.__size)
