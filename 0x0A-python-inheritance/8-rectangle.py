4787103a2cc6e90e028fe954b37bc2d35fe67377
tsims115 <tsims115@yahoo.com>
2022-01-20 17:43:55 -0800

    taking out if statements

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,8 +3,11 @@
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
+    """Class Rectangle inherits from BaseGeometry"""
+
     def __init__(self, width, height):
-        if super().integer_validator("width", width):
-            self.__width = width
-        if super().integer_validator("height", height):
-            self.__height = height
+        """Initializes with width and height"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
