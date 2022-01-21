db1dd51f9f83105b80698950665979c6940c816d
tsims115 <tsims115@yahoo.com>
2022-01-20 17:48:56 -0800

    fixing pepe and adding docs

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,14 +3,22 @@
 
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Class Rectangle inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-        if super().integer_validator("width", width):
-            self.__width = width
-        if super().integer_validator("height", height):
-            self.__height = height
+        """initializes with width and height"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
 
     def area(self):
+        """returns area with height and width"""
         return self.__height * self.__width
+
+    def __str__(self):
+        """string representation of object"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
