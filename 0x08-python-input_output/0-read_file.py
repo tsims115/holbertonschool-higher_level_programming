0e017c3ef987c8d7d10c663de397d6e1a6176777
tsims115 <tsims115@yahoo.com>
2022-01-21 16:35:38 -0800

    Adding task 0

---
+++ 0x08-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 0-read_file.py"""
+
+
+def read_file(filename=""):
+    """Function read_file - prints to stdout content of file
+        filename: name of file to read
+    """
+    with open(filename, 'r', encoding = "utf8") as f:
+        for line in f:
+            print("{}".format(line))
