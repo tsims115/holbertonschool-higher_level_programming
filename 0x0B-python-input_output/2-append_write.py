cf1975aa402fef611133ea6c649aae302360e452
tsims115 <tsims115@yahoo.com>
2022-01-23 09:14:37 -0800

    Adding task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,9 +2,6 @@
 """Module 2-append_write.py with function append_write"""
 
 
-from re import L
-
-
 def append_write(filename="", text=""):
     """append_write - appends a string at the end of a text file
         filename: file to append to
