40aab62c6a3a30a26a68b6e1c0c89f33d0581513
tsims115 <tsims115@yahoo.com>
2022-01-23 16:16:56 -0800

    Adding task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,11 +2,8 @@
 """Module 8-class_to_json with function class_to_json"""
 
 
-import json
-
-
 def class_to_json(obj):
     """class_to_json - returns dict description
         obj: class instance to get dict description of
     """
-    return json.dumps(str(obj))
+    return obj.__dict__
