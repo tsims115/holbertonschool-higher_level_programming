1fa5cdf3d55bfba42565111835db85fa62897f20
tsims115 <tsims115@yahoo.com>
2022-01-26 13:03:31 -0800

    Adding staticmethod to convert to json strings

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,11 @@
 """Module models.base with base class"""
 
 
+import json
+
+
 class Base:
+    """Class Base that instanciates with id"""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -13,3 +17,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
