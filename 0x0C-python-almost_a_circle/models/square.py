105a3b3dbf07890f7f68d8ca13788a3a100993df
tsims115 <tsims115@yahoo.com>
2022-01-26 10:26:02 -0800

    Adding update method to square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,28 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    def update(self, *args, **kwargs):
+        count = 0
+        for i in args:
+            if count == 0:
+                self.id = i
+            elif count == 1:
+                self.size = i
+            elif count == 2:
+                self.x = i
+            elif count == 3:
+                self.y = i
+            count += 1
+        if args:
+            return
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                if k == "size":
+                    self.integer_validator("width", v)
+                else:
+                    self.integer_validator(k, v)
+                setattr(self, k, v)
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
