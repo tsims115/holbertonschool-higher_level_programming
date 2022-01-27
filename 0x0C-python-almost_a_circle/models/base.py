268711db035636b32e814a79612c8f3c6a7cf48a
tsims115 <tsims115@yahoo.com>
2022-01-26 16:39:08 -0800

    static method to get dict from JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,8 +25,16 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list of the JSON string representation"""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
+        """Saves a list of object's dictionary representation to a file"""
         json_list = []
         if list_objs is not None:
             for obj in list_objs:
