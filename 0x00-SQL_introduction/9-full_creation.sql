e83dc34c0c6eaf3c13ba7871a2cc0f9ee8167756
tsims115 <tsims115@yahoo.com>
2022-02-09 14:03:57 -0800

    Adding task 9

---
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Creates table second_table in database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUE
+       (1, "John", 10),
+       (2, "Alex", 3),
+       (3, "Bob", 14),
+       (4, "George", 8);
