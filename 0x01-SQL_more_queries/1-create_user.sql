efd3305e52422d83691410aebd8b8377029f365b
tsims115 <tsims115@yahoo.com>
2022-02-10 13:05:04 -0800

    Adding comment

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
+-- creates the user user_0d_0 with all privileges
 CREATE USER IF NOT EXISTS  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
