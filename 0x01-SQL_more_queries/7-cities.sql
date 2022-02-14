c38af2daf511177c6fe4e0f2b42e2c590c4b2fee
tsims115 <tsims115@yahoo.com>
2022-02-14 08:04:42 -0800

    Fixing typo!

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY (id),
-    state_id INT NOT NULL FOREIGN KEY (id),
-    name VARCHAR(256) NOT NULL
-    PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERANCES states(id)
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
