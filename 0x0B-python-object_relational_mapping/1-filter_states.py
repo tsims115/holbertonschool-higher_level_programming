95a6b68e99052890e0d173db801b4429b3aa8cd8
tsims115 <tsims115@yahoo.com>
2022-02-22 14:56:22 -0800

    Adding task 012

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 0-select_states with function"""
+"""Module 1-filter_states.py with function"""
 
 from ast import arg
 import MySQLdb
@@ -10,11 +10,12 @@ db = MySQLdb.connect(
     host="localhost",
     user=sys.argv[1],
     passwd=sys.argv[2],
-    db=sys.argv[3]
+    db=sys.argv[3],
     )
 cur = db.cursor()
 numrows = cur.execute("""
     SELECT id, name FROM states
+    WHERE name LIKE 'N%'
     ORDER BY states.id ASC
     """)
 for i in range(numrows):
--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 0-select_states with function"""
+"""Module 1-filter_states.py with function"""
 
 from ast import arg
 import MySQLdb
@@ -10,12 +10,13 @@ db = MySQLdb.connect(
     host="localhost",
     user=sys.argv[1],
     passwd=sys.argv[2],
-    db=sys.argv[3]
+    db=sys.argv[3],
     )
 cur = db.cursor()
+state_name = "'" + sys.argv[4] + "'"
 numrows = cur.execute("""
     SELECT id, name FROM states
-    ORDER BY states.id ASC
-    """)
+    WHERE name = %s ORDER BY states.id ASC
+    """ %  state_name)
 for i in range(numrows):
     print("(%s, '%s')" % cur.fetchone())
