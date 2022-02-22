25caabd55fefd1b3be84bca5c6cde6112fa448fa
tsims115 <tsims115@yahoo.com>
2022-02-22 14:26:31 -0800

    Adding task 0

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module 0-select_states with function"""
+
+from ast import arg
+import MySQLdb
+import sys
+
+
+db = MySQLdb.connect(
+    host="localhost",
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+    )
+cur = db.cursor()
+numrows = cur.execute("SELECT id, name FROM states")
+for i in range(numrows):
+    print("(%s, '%s')" % cur.fetchone())
