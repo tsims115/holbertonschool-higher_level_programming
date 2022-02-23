83066104312cc67289ad84990f3c5c8cd602bbb1
tsims115 <tsims115@yahoo.com>
2022-02-23 14:12:08 -0800

    Adding task 1

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     import sys
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
