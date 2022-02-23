4c2d12caa17900b4528bde3938cccf5c736f8b77
tsims115 <tsims115@yahoo.com>
2022-02-23 13:52:06 -0800

    Adding task 1 and 2 main

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """Module 1-filter_states.py with function"""
 
-from ast import arg
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
 
-db = MySQLdb.connect(
-    host="localhost",
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    )
-cur = db.cursor()
-numrows = cur.execute("""
-    SELECT id, name FROM states
-    WHERE name LIKE 'N%'
-    ORDER BY states.id ASC
-    """)
-for i in range(numrows):
-    print("(%s, '%s')" % cur.fetchone())
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        )
+    cur = db.cursor()
+    numrows = cur.execute("""
+        SELECT id, name FROM states
+        WHERE name LIKE 'N%'
+        ORDER BY states.id ASC
+        """)
+    for i in range(numrows):
+        print("(%s, '%s')" % cur.fetchone())
+    cur.close()
+    db.close()
--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """Module 1-filter_states.py with function"""
 
-from ast import arg
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
 
-db = MySQLdb.connect(
-    host="localhost",
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    )
-cur = db.cursor()
-state_name = "'" + sys.argv[4] + "'"
-numrows = cur.execute("""
-    SELECT id, name FROM states
-    WHERE name = %s ORDER BY states.id ASC
-    """ %  state_name)
-for i in range(numrows):
-    print("(%s, '%s')" % cur.fetchone())
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        )
+    cur = db.cursor()
+    state_name = "'" + sys.argv[4] + "'"
+    numrows = cur.execute("""
+        SELECT id, name FROM states
+        WHERE name = %s ORDER BY states.id ASC
+        """ %  state_name)
+    for i in range(numrows):
+        print("(%s, '%s')" % cur.fetchone())
+    cur.close
+    db.close
