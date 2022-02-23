fe318dbae7eee59368b93ce793ba22be5fa7a7b0
tsims115 <tsims115@yahoo.com>
2022-02-23 14:14:42 -0800

    Adding task 1

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
+        charset="utf8"
         )
     cur = db.cursor()
     numrows = cur.execute("""
