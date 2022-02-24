8aff1a7e248520c065c5a9bd99965318ad9d9ffa
tsims115 <tsims115@yahoo.com>
2022-02-23 23:48:31 -0800

    Adding task 10

---
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Start link class to table in database and fetch states
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        if sys.argv[4] == instance.name:
+            print("{}".format(instance.id))
