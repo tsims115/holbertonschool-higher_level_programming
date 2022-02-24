0ac3424213756b80461362e59529589bba7b5b7a
tsims115 <tsims115@yahoo.com>
2022-02-23 16:53:45 -0800

    Adding task 2

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Module 1-filter_states.py with function"""
+"""Module 2-my_filter_states.py with function"""
 
 if __name__ == "__main__":
+
     import MySQLdb
     import sys
+
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
+        charset="utf8"
         )
     cur = db.cursor()
     state_name = "'" + sys.argv[4] + "'"
@@ -17,6 +21,6 @@ if __name__ == "__main__":
         WHERE name = %s ORDER BY states.id ASC
         """ % state_name)
     for i in range(numrows):
-        print("(%s, '%s')" % cur.fetchone())
+        print(cur.fetchone())
     cur.close
     db.close
