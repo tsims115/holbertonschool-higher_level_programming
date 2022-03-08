aa527ed309bee9d47c5ba75fb0f91249c9f2f311
tsims115 <tsims115@yahoo.com>
2022-03-08 12:19:20 -0800

    Adding task 0

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,7 +6,4 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
-    print("Body repsonse:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf8')))
+    print(html)
