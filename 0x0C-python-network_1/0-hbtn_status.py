f7b8dd70ff26cb1d666f4b1acf2ed293a8fcc59c
tsims115 <tsims115@yahoo.com>
2022-03-08 14:43:57 -0800

    Fixing print statement

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     with request.urlopen(url) as response:
         html = response.read()
-        print("Body repsonse:")
+        print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf8')))
