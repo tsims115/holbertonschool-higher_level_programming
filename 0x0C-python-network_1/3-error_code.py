72cf949f434ab7a355091dbeaf1e8ec0ae283df7
tsims115 <tsims115@yahoo.com>
2022-03-08 12:28:47 -0800

    Adding task 3

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -5,10 +5,14 @@
 import sys
 import urllib.request
 import urllib.parse
+import urllib.error
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        print("{}".format(response.read().decode('utf8')))
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            print("{}".format(response.read().decode('utf8')))
+    except urllib.error.URLError as e:
+        print(e.reason)
