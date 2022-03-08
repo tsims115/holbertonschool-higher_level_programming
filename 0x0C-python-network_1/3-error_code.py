60ed509646bc5147c8deeb95797a06e2ea8ce0d8
tsims115 <tsims115@yahoo.com>
2022-03-08 11:51:49 -0800

    Addigng task 3

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 2-post_email.py sends a POST"""
+
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print("Your email is: {}".format(response.read().decode('utf8')))
