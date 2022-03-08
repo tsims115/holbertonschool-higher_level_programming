3e1811fce72205d3740b33f22876887833423956
tsims115 <tsims115@yahoo.com>
2022-03-08 11:42:47 -0800

    Adding task 2

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module 2-post_email.py sends a POST"""
+
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {'email': email}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print("Your email is: {}".format(response.read().decode('utf8')))
