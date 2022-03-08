4e9c405a43d25104650112d54cf36b943e194301
tsims115 <tsims115@yahoo.com>
2022-03-08 14:38:12 -0800

    Adding urlerror instead

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
         req = urllib.request.Request(url)
         with urllib.request.urlopen(req) as response:
             print("{}".format(response.read().decode('utf8')))
-    except urllib.error.HTTPError as e:
+    except urllib.error.URLError as e:
         print("Error code: {}".format(e.reason))
