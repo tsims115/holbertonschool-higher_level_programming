2d994c77bfab05821d4ff7210899abddc19ff9a2
tsims115 <tsims115@yahoo.com>
2022-03-08 17:45:36 -0800

    Adding task 7

---
+++ 0x0C-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 4-hbtn_status fethces https://intranet.hbtn.io/status"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.post(url)
+    code = r.status_code
+    if code >= 400:
+        print("Error code: {}".format(code))
