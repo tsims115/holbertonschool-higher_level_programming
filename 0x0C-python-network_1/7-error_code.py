dd452136a6ee257a46d59406b80015bdad5c6aac
tsims115 <tsims115@yahoo.com>
2022-03-08 17:48:06 -0800

    now printing r.text

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    r = requests.post(url)
+    r = requests.get(url)
     code = r.status_code
     if code >= 400:
         print("Error code: {}".format(code))
