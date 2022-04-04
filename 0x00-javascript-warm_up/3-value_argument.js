d2352cabbddf4de0536f839991b91a4f11e4bb84
tsims115 <tsims115@yahoo.com>
2022-04-04 10:42:04 -0700

    Fixing semistandard

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 let first_arg = process.argv[2];
 console.log(first_arg);
-if (first_arg === undefined)
-{
-    console.log("No argument")
-}
-else
-{
-    console.log("Shool")
+if (first_arg === undefined) {
+  console.log('No argument')
+} else {
+  console.log('School')
 }
