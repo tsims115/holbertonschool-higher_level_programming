34027155ca9c4229a98ceac6265f1008aa314c59
tsims115 <tsims115@yahoo.com>
2022-04-04 11:52:02 -0700

    Adding task 10

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,2 +1,19 @@
-11-second_biggest.js
+#!/usr/bin/node
 
+function factorial (num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+const args = process.argv;
+let int1;
+if (args[2]) {
+  int1 = parseInt(args[2]);
+}
+if (isNaN(int1)) {
+  int1 = 1;
+}
+console.log(factorial(int1));
