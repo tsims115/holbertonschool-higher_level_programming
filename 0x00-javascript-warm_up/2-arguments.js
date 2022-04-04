f09950c1e203484bd5cc427ed0e0686781f38e22
tsims115 <tsims115@yahoo.com>
2022-04-04 01:05:21 -0700

    Adding first 3 tasks

---
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let args = process.argv ;
+len = args.length;
+if (len === 2)
+{
+    console.log("No argument");
+}
+else if (len === 3)
+{
+    console.log("Argument found")
+}
+else
+{
+    console.log("Arguments found")
+}
