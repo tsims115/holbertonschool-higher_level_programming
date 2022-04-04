23548b22e8559a036156a413e8676a2686490729
tsims115 <tsims115@yahoo.com>
2022-04-04 11:43:06 -0700

    Adding task 9

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 
+function add (a, b) {
+  return a + b;
+}
+
+const args = process.argv;
+let int1 = NaN;
+let int2 = NaN;
+if (args[2]) {
+  int1 = parseInt(args[2]);
+}
+if (args[3]) {
+  int2 = parseInt(args[3]);
+}
+console.log(add(int1, int2));
