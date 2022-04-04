12fc951cedfc9244f324dc76024e8f975136e13e
tsims115 <tsims115@yahoo.com>
2022-04-04 10:47:25 -0700

    Adding task 3

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let first_arg = process.argv[2];;
-if (first_arg === undefined) {
-  console.log('No argument')
+const firstArg = process.argv[2];
+if (firstArg === undefined) {
+  console.log('No argument');
 } else {
-  console.log(first_arg)
+  console.log(firstArg);
 }
