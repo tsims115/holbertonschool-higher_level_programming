849db10dfd01a517e6bcad3f6f92178769fdc480
tsims115 <tsims115@yahoo.com>
2022-04-04 12:13:28 -0700

    Adding task 11

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,28 @@
 #!/usr/bin/node
 
+function max (a) {
+  let max = a[0];
+  let secondMax = a[0];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > max) {
+      max = a[i];
+    }
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > secondMax && a[i] < max) {
+      secondMax = a[i];
+    }
+  }
+  return (secondMax);
+}
+
+const args = process.argv;
+const array = [];
+for (let i = 2; args[i]; i++) {
+  array.push(parseInt(args[i]));
+}
+if (array.length <= 1) {
+  console.log(0);
+} else {
+  console.log(max(array));
+}
