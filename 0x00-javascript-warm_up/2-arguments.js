f756c714de8d76b741ead284711a7c87d891a2fb
tsims115 <tsims115@yahoo.com>
2022-04-04 10:25:17 -0700

    Fixing semistandard

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-let args = process.argv ;
-len = args.length;
-if (len === 2)
-{
-    console.log("No argument");
-}
-else if (len === 3)
-{
-    console.log("Argument found");
-}
-else
-{
-    console.log("Arguments found");
+const args = process.argv;
+const len = args.length;
+if (len === 2) {
+  console.log('No argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
