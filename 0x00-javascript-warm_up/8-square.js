4332fb54597e5248d4054b4624b93df69b19abdf
tsims115 <tsims115@yahoo.com>
2022-04-04 11:31:46 -0700

    Adding task 8

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,2 +1,22 @@
 #!/usr/bin/node
-
+const args = process.argv;
+const len = args.length;
+let int;
+let flag = 0;
+let row;
+if (len === 3) {
+  int = parseInt(args[2]);
+} else {
+  flag = 1;
+}
+if (isNaN(int) || flag === 1) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < int; i++) {
+    row = '';
+    for (let j = 0; j < int; j++) {
+      row += '#';
+    }
+    console.log(row);
+  }
+}
