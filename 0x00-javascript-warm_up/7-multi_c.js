78c78da84878ad04f22d5d3d53fc710a5d9ef35b
tsims115 <tsims115@yahoo.com>
2022-04-04 11:15:50 -0700

    Addign task 7

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
-
+const args = process.argv;
+const len = args.length;
+let int;
+let flag = 0;
+if (len === 3) {
+  int = parseInt(args[2]);
+} else {
+  flag = 1;
+}
+if (isNaN(int) || flag === 1) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let idx = 0; idx < int; idx++) {
+    console.log('C is fun');
+  }
+}
