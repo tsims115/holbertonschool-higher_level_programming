1c1fde60bbbdf9d2450122d28091a7c557e2738a
tsims115 <tsims115@yahoo.com>
2022-04-07 20:29:53 -0700

    Adding task 4

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const urlToFind = 'https://swapi-api.hbtn.io/api/people/18/';
 let count = 0;
 request(url, function (e, response, body) {
   if (e) {
@@ -10,7 +9,7 @@ request(url, function (e, response, body) {
   const data = JSON.parse(body).results;
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data[i].characters.length; j++) {
-      if (data[i].characters[j] === urlToFind) {
+      if (data[i].characters[j].includes('18')) {
         count++;
       }
     }
