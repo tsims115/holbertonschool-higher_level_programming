629424dc31460660b4a8623af0a7d3e346816bbb
tsims115 <tsims115@yahoo.com>
2022-04-07 21:14:24 -0700

    making it make an object

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-let userTasks = {}
+const userTasks = {};
 request(url, function (e, response, body) {
   if (e) {
     console.error('error:', e);
   }
-  data = JSON.parse(body);
+  const data = JSON.parse(body);
   for (let i = 0; i < data.length; i++) {
-    let task = data[i];
-    let id = data[i].userId;
+    const task = data[i];
+    const id = data[i].userId;
     if (task.completed === true) {
       if (!userTasks[id]) {
         userTasks[id] = 0;
