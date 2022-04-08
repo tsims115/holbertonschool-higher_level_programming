b8c97503365d394d152be2177bb4576ab0ca46c6
tsims115 <tsims115@yahoo.com>
2022-04-07 21:09:54 -0700

    Adding task 6

---
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let userTasks = {}
+request(url, function (e, response, body) {
+  if (e) {
+    console.error('error:', e);
+  }
+  data = JSON.parse(body);
+  for (let i = 0; i < data.length; i++) {
+    let task = data[i];
+    let id = data[i].userId - 1;
+    if (task.completed === true) {
+      if (!userTasks[id]) {
+        userTasks[id] = 0;
+      }
+      userTasks[id] += 1;
+    }
+  }
+  console.log(userTasks);
+});
