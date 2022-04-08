847e986e3be64d9ce28a03b479133f675968f452
tsims115 <tsims115@yahoo.com>
2022-04-07 19:03:13 -0700

    Using it from commnd line

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 const filename = process.argv[2];
-fs.readFile('cisfun', function (err, contents) {
-    console.log(err);
-    console.log(contents.toString());
+fs.readFile(filename, function (err, contents) {
+  console.log(err);
+  console.log(contents.toString());
 });
